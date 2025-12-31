78e3672f8d1afc6289dd51c957708499044c1894
ashoknandurka <ashoknandurka0104@gmail.com>
2025-12-31 13:21:38 +0530

    palindrome changes

--- strings/polindrome.js
+++ strings/polindrome.js
@@ -7,3 +7,40 @@ function isPalindrome(str){
 const str = 'ashok, kohsa'
  const palindrome = isPalindrome(str)
  console.log("ispalindrome::",palindrome)
+
+ // two pointer method
+
+ function isPalindrome(str) {
+    str = str.toLowerCase().replace(/[^a-z0-9]/g, '');   // ignoring case and special charactors (do only when asked)
+
+    let left = 0;
+    let right = str.length - 1;
+  
+    while (left < right) {
+      if (str[left] !== str[right]) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+    return true;
+  }
+
+  isPalindrome("A man, a plan, a canal: Panama"); // true
+
+  // For Numbers
+
+  function isPalindrome(num) {
+    let original = num;
+    let reversed = 0;
+  
+    while (num > 0) {
+      reversed = reversed * 10 + (num % 10);
+      num = Math.floor(num / 10);
+    }
+    return original === reversed;
+  }
+  
+  
+
+
